f885b994dfc54d5bece04eb62a4471951c74b4db
Gabe Scholz <scholz.gabe@gmail.com>
2017-05-14 14:08:06 -0700

    Initial commit

---
+++ index.js
@@ -0,0 +1,22 @@
+const pkgUp = require('pkg-up');
+const cp = require('child_process');
+
+const fetch = 'git fetch origin master';
+const currentBranchName = 'git rev-parse --abbrev-ref HEAD';
+const porcelain = 'git status --porcelain';
+const upToDate = 'git diff HEAD FETCH_HEAD';
+
+module.exports = () =>
+  pkgUp().then(pkg => {
+    const cwd = pkg.split('package.json')[0];
+
+    const exec = cmd =>
+      new Promise((resolve, reject) =>
+        cp.exec(cmd, { cwd }, (err, stdout) => err ? reject(err) : resolve(stdout))
+      );
+
+    return exec(fetch).then(() => exec(currentBranchName))
+      .then(master => master === 'master\n' ? exec(porcelain) : Promise.reject(false))
+      .then(empty => empty === '' ? exec(upToDate) : Promise.reject(false))
+      .then(empty => empty === '' ? Promise.resolve(true) : Promise.reject(false));
+  });
